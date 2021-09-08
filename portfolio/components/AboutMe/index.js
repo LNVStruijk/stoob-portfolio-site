e0281c76e1af1a56c2586db853b62ac8e397feee
andreavillang <andreavillang@gmail.com>
2021-09-08 18:34:34 +0800

    shortcut template ok

--- portfolio/components/AboutMe/index.js
+++ portfolio/components/AboutMe/index.js
@@ -1,16 +1,16 @@
 //HTML n JAVASCRIPT location for AboutMe page
 
-import styles from './AboutMe.module.css';
+import styles from "./AboutMe.module.scss";
 
 const AboutMe = (props) => {
-    // this is component so we want to make it constant 
-    //'props' is a parameter that you can change or smth
-    return (
-        <>
-            <h1>FIVENGIHTSATFREEYSDIEEEEEEEEEEEEEES</h1>
-            <p>{props.faggot}</p>
-        </>
-    )
-}
+  // this is component so we want to make it constant
+  //'props' is a parameter that you can change or smth
+  return (
+    <>
+      <h1>FIVENGIHTSATFREEYSDIEEEEEEEEEEEEEES</h1>
+      <p>{props.faggot}</p>
+    </>
+  );
+};
 
 export default AboutMe;
--- portfolio/components/Shortcut/Index.js
+++ portfolio/components/Shortcut/Index.js
@@ -1,24 +1,24 @@
-import Google from '../../assets/svg/Google';
-import Image from 'next/image';
-import ShortcutImg from '../../public/msagent-1.png';
-import styles from './Shortcut.module.scss';
+import Google from "../../assets/svg/Google";
+import styles from "./Shortcut.module.scss";
 
 const Shortcut = (props) => {
-    // this is component so we want to make it constant 
-    //'props' is a parameter that you can change or smth
-    return (
-        <div className = {styles.container}>
-            <div className={styles.logo}>
-                <Image src={ShortcutImg} alt="Vercel Logo" width={"100%"} height={"100%"} />
-            </div>
-            
-            <h1 className={styles.name}>{props.shortcutName}</h1> 
-            {/* it's okay to leave 'props' blank, this can be edited outside */}
-            
-            {/* <img src="pic_trulli.jpg" alt="Italian Trulli"> */}
+  // this is component so we want to make it constant
+  //'props' is a parameter that you can change or smth
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        { props.image === "google" ? (
+          <Google height={'100%'}/>
+        ) : (
+          null
+        )}
+        
+      </div>
 
-        </div>
-    )
-}
+      <h1>{props.name}</h1>
+      {/* it's okay to leave 'props' blank, this can be edited outside */}
+    </div>
+  );
+};
 
 export default Shortcut;
