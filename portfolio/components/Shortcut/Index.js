ebf642d72d95e4f8f7c0ed887be499ee0d06e70b
andreavillang <andreavillang@gmail.com>
2021-09-09 19:33:19 +0800

    shortcut template done

--- portfolio/components/Shortcut/Index.js
+++ portfolio/components/Shortcut/Index.js
@@ -1,27 +1,38 @@
 import Google from "../../assets/svg/Google";
 import styles from "./Shortcut.module.scss";
 import Image from "next/image";
-import SmilingDude from "../../public/images/SmilingDude.gif";
-import doomGuy from "../../public/images/doomGuyAnim.gif";
+import SmilingDude from "../../public/images/smiling-dude.gif";
+import DoomGuy from "../../public/images/doom-guy-anim.gif";
 
 const Shortcut = (props) => {
   // this is component so we want to make it constant
   //'props' is a parameter that you can change or smth
+
+  const iconSize = 100;
+
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
         {props.image === "image1" ? (
-          <Image src={SmilingDude} width={100} height={100}/>
-        ) :props.image === "image2" ? 
-        (<Image src={doomGuy} width={100} height={100}/>):
-        null }
+          <Image src={SmilingDude} width={iconSize} height={iconSize} />
+        ) : props.image === "image2" ? (
+          <Image src={DoomGuy} width={iconSize} height={iconSize} />
+        ) : props.image === "image3" ? (
+          <Image src={DoomGuy} width={iconSize} height={iconSize} />
+        ) : props.image === "image4" ? (
+          <Image src={DoomGuy} width={iconSize} height={iconSize} />
+        ) : props.image === "image5" ? (
+          <Image src={DoomGuy} width={iconSize} height={iconSize} />
+        ) : props.image === "image6" ? (
+          <Image src={DoomGuy} width={iconSize} height={iconSize} />
+        ) : props.image === "image7" ? (
+          <Image src={DoomGuy} width={iconSize} height={iconSize} />
+        ) : null }
       </div>
 
       <div className={styles.textContainer}>
         <h1>{props.name}</h1>
       </div>
-
-      {/* it's okay to leave 'props' blank, this can be edited outside */}
     </div>
   );
 };
