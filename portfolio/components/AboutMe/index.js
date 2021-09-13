f6f11236302bf14093dc963abee75dd7acbe1853
stoobas wife <lucasstruijk@gmail.com>
2021-09-14 01:02:38 +0800

    gallery

--- portfolio/components/AboutMe/index.js
+++ portfolio/components/AboutMe/index.js
@@ -1,13 +1,78 @@
 //HTML n JAVASCRIPT location for AboutMe page
+import Image from 'next/image'
 
 import styles from "./AboutMe.module.scss";
+import BackgroundImage from "../../public/images/planet-earth.gif"
+
 
 const AboutMe = (props) => {
   // this is component so we want to make it constant
   //'props' is a parameter that you can change or smth
   return (
     <>
-      <h1>FIVENGIHTSATFREEYSDIEEEEEEEEEEEEEES</h1>
+      <div className={styles.container}>
+        {/* BACKGROUND */}
+        {/* <div className={styles.background}>
+          <Image src={BackgroundImage}/>
+        </div> */}
+        {/* LEFT CONTAINER */}
+        <div className={styles.leftContainer}>
+          <div className={styles.greenBox}>
+            <h2>Name: Lucas Struijk</h2>
+            <h2>Birth: 12/11/1996</h2>
+          </div>
+          <div className={styles.greenBox}>
+            <h2>Profession</h2>
+            <h2>Graphic Designer</h2>
+          </div>
+          <div className={styles.greenBox}>
+            <div className={styles.portrait}></div>
+          </div>
+        </div>
+        {/* RIGHT CONTAINER */}
+        <div className={styles.rightContainer}>
+          <div className={styles.greenBox}>
+            <h1>SKILLS</h1>
+          </div>
+          <div className={styles.greenBox}>
+            <div className={styles.skillContainer}>
+              <h2>Poster Design</h2>
+              <h2>TRAINED</h2>
+            </div>
+            <div className={styles.skillContainer}>
+              <h2>Web Design</h2>
+              <h2>TRAINED</h2>
+            </div>
+            <div className={styles.skillContainer}>
+              <h2>Oil Painting</h2>
+              <h2>TRAINED</h2>
+            </div>
+            <div className={styles.skillContainer}>
+              <h2>Digital Painting</h2>
+              <h2>TRAINED</h2>
+            </div>
+            <div className={styles.skillContainer}>
+              <h2>Watercolor</h2>
+              <h2>TRAINED</h2>
+            </div>
+            <div className={styles.skillContainer}>
+              <h2>Magazine Design</h2>
+              <h2>TRAINED</h2>
+            </div>
+            <div className={styles.skillContainer}>
+              <h2>Unity Engine</h2>
+              <h2>TRAINED</h2>
+            </div>
+          </div>
+          <div className={styles.greenBox}>
+            <p>
+              Lucas is driven by finding new ways to improve his skills in
+              graphic design and the art world. He seems to enjoy learning new
+              skills depending on the tasks given to him.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- portfolio/components/GalleryView/index.js
+++ portfolio/components/GalleryView/index.js
@@ -1,13 +1,59 @@
 import styles from "./GalleryView.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+import Stalker from "../../public/images/stalker.png";
+import Vertigo from "../../public/images/vertigo-poster.png";
+import Radiation from "../../public/images/radiation2.png";
+import Williams from "../../public/images/ash-williams.png";
+import Shining from "../../public/images/shining-poster.png";
+import BurnVictim from "../../public/images/burn-victim.png";
+import Blood from "../../public/images/blood-poster.png";
+import Tortoise from "../../public/images/tortoise.jpg";
 
 const galleryView = (props) => {
-    return(
-        <>
-            <p>
-                GALLERY CONTENTS
-            </p>
-        </>
-    )
-}
+  const imgSize = 200;
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.flexRow}>
+          <div className={styles.stalker + " " + styles.images}>
+            <a
+              href="https://www.youtube.com/watch?v=MFujRMSjWnE"
+              target="_blank"
+            >
+              <Image src={Stalker} />
+            </a>
+          </div>
+          <div className={styles.vertigo + " " + styles.images}>
+            <Image src={Vertigo} />
+          </div>
+          <div className={styles.radiation + " " + styles.images}>
+            <Image src={Radiation} />
+          </div>
+        </div>
+        <div className={styles.flexRow}>
+          <div className={styles.williams + " " + styles.images}>
+            <Image src={Williams} />
+          </div>
+          <div className={styles.shining + " " + styles.images}>
+            <Image src={Shining} />
+          </div>
+          <div className={styles.burnVictim + " " + styles.images}>
+            <Image src={BurnVictim} />
+          </div>
+        </div>
+        <div className={styles.flexRow}>
+          <div className={styles.blood + " " + styles.images}>
+            <Image src={Blood} />
+          </div>
+          <div className={styles.tortoise + " " + styles.images}>
+            <Image src={Tortoise} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default galleryView;
