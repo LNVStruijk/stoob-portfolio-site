597a04ee819d8bc21fcf9bcac6afc873250b7b88
stoobas wife <lucasstruijk@gmail.com>
2021-09-24 14:03:56 +0800

    gallery links

--- portfolio/components/GalleryView/index.js
+++ portfolio/components/GalleryView/index.js
@@ -32,46 +32,101 @@ const galleryView = (props) => {
             </a>
           </div>
           <div className={styles.vertigo + " " + styles.images}>
-            <Image src={Vertigo} />
+            <a
+              href="https://drive.google.com/file/d/1zmxn3moPF8Gm3UOHw2Jjru4cmN6cpTix/view?usp=sharing"
+              target="_blank"
+            >
+              <Image src={Vertigo} />
+            </a>
           </div>
           <div className={styles.radiation + " " + styles.images}>
+          <a
+              href="https://drive.google.com/file/d/1ec7PMKk7FGdMIfhsLSIVkGfLiR9nhBy0/view?usp=sharing"
+              target="_blank"
+            >
             <Image src={Radiation} />
+            </a>
           </div>
         </div>
         {/* ROW 2 */}
         <div className={styles.flexRow}>
           <div className={styles.shining + " " + styles.images}>
+          <a
+              href="https://drive.google.com/file/d/1U1FIcQh0mCDIylKcvw9pM2hrVlLzyI2t/view?usp=sharing"
+              target="_blank"
+            >
             <Image src={Shining} />
+            </a>
           </div>
           <div className={styles.williams + " " + styles.images}>
+          <a
+              href="https://drive.google.com/file/d/1JPFo6WKsbDhX7FsDhNVFXYYxOjOc87fG/view?usp=sharing"
+              target="_blank"
+            >
             <Image src={Williams} />
+            </a>
           </div>
           <div className={styles.burnVictim + " " + styles.images}>
+          <a
+              href="https://drive.google.com/file/d/1mMHOQqsLbZ3k0lQFB_rCfXazcHkNJOtQ/view?usp=sharing"
+              target="_blank"
+            >
             <Image src={BurnVictim} />
+            </a>
           </div>
         </div>
         {/* ROW 3 */}
         <div className={styles.flexRow}>
           <div className={styles.blood + " " + styles.images}>
+          <a
+              href="https://drive.google.com/file/d/1s_HggDR7-sRF1A4GmpZDzxQevjPhkv4g/view?usp=sharing"
+              target="_blank"
+            >
             <Image src={Blood} />
+            </a>
           </div>
           <div className={styles.tortoise + " " + styles.images}>
+          <a
+              href="https://drive.google.com/file/d/1DLuVSGGeByaCGQxCDcOe_VeauDLacf1c/view?usp=sharing"
+              target="_blank"
+            >
             <Image src={Tortoise} />
+            </a>
           </div>
           <div className={styles.baby + " " + styles.images}>
+          <a
+              href="https://drive.google.com/file/d/1BacQuo4TRiacZYgzNA1Z1R0H8COmd18g/view?usp=sharing"
+              target="_blank"
+            >
             <Image src={Baby} />
+            </a>
           </div>
         </div>
         {/* ROW 4 */}
         <div className={styles.flexRow}>
           <div className={styles.whiplash + " " + styles.images}>
+          <a
+              href="https://drive.google.com/file/d/1EOo4Ul8v_Tg8c5z1nmKqEAVSvsJ8F2bc/view?usp=sharing"
+              target="_blank"
+            >
             <Image src={Whiplash} />
+            </a>
           </div>
           <div className={styles.reptile + " " + styles.images}>
+          <a
+              href="https://drive.google.com/file/d/1z6l12fP9db2r4ECLBB8pU4tZtqC7EBQg/view?usp=sharing"
+              target="_blank"
+            >
             <Image src={Reptile} />
+            </a>
           </div>
           <div className={styles.oldboy + " " + styles.images}>
+          <a
+              href="https://drive.google.com/file/d/1JFpIzT5wW9YVpVsm0lZcKperEFlXZ6Tb/view?usp=sharing"
+              target="_blank"
+            >
             <Image src={OldBoy} />
+            </a>
           </div>
         </div>
       </div>
