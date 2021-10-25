eb1908dbf2f71fab0a2f886965f7c2c24b7ee228
BawalBakla420 <lucasstruijk@gmail.com>
2021-10-25 15:00:53 +0800

    added magazine spread

--- portfolio/components/Typography/index.js
+++ portfolio/components/Typography/index.js
@@ -5,6 +5,10 @@ import DitherTexture from "../../public/images/dither-texture.png";
 import Droid from "../../public/images/typography-low-res/droid.png";
 import Circuit from "../../public/images/typography-low-res/circuit.jpg";
 import CenturyGothic from "../../public/images/typography-low-res/century-gothic.jpg";
+import Spread1 from "../../public/images/typography-low-res/mag-spread-1.jpg";
+import Spread2 from "../../public/images/typography-low-res/mag-spread-2.jpg";
+import Spread3 from "../../public/images/typography-low-res/mag-spread-3.jpg";
+import Spread4 from "../../public/images/typography-low-res/mag-spread-4.jpg";
 import ReactTooltip from "react-tooltip";
 import { isMobile } from "react-device-detect";
 import React, { useState, useEffect } from "react";
@@ -29,7 +33,7 @@ const Typography = (props) => {
           {/* ROW 1 */}
           <div
             data-tip={
-              '<h1>"Droid Font Poster"</h1> <h4> PaintToolSAI </h4> <p>Inspired by Tarkovky\'s Stalker (1979)</p> <h5>Click to see full image</h5>'
+              '<h1>"Droid Typeface Poster"</h1> <h4> PaintToolSAI </h4> <p>Inspired by Tarkovky\'s Stalker (1979)</p> <h5>Click to see full image</h5>'
             }
             data-html={true}
             className={styles.droid + " " + styles.images}
@@ -60,7 +64,7 @@ const Typography = (props) => {
               '<h1>"Droid Font Poster"</h1> <h4> PaintToolSAI </h4> <p>Inspired by Tarkovky\'s Stalker (1979)</p> <h5>Click to see full image</h5>'
             }
             data-html={true}
-            className={styles.circuit + " " + styles.images}
+            className={styles.centuryGothic + " " + styles.images}
           >
             <a
               href="https://drive.google.com/file/d/14v2qsZ__RR7-m-zFJi-BLD9GWwjc7PCS/view?usp=sharing"
@@ -71,9 +75,67 @@ const Typography = (props) => {
           </div>
         </div>
         {/* ROW 2 */}
-        <div className={styles.flexRow}></div>
+        <div className={styles.flexRow}>
+          <div
+            data-tip={
+              '<h1>"Droid Font Poster"</h1> <h4> PaintToolSAI </h4> <p>Inspired by Tarkovky\'s Stalker (1979)</p> <h5>Click to see full image</h5>'
+            }
+            data-html={true}
+            className={styles.Spread1 + " " + styles.images}
+          >
+            <a
+              href="https://drive.google.com/file/d/14v2qsZ__RR7-m-zFJi-BLD9GWwjc7PCS/view?usp=sharing"
+              target="_blank"
+            >
+              <Image src={Spread1} />
+            </a>
+          </div>
+          <div
+            data-tip={
+              '<h1>"Droid Font Poster"</h1> <h4> PaintToolSAI </h4> <p>Inspired by Tarkovky\'s Stalker (1979)</p> <h5>Click to see full image</h5>'
+            }
+            data-html={true}
+            className={styles.Spread1 + " " + styles.images}
+          >
+            <a
+              href="https://drive.google.com/file/d/14v2qsZ__RR7-m-zFJi-BLD9GWwjc7PCS/view?usp=sharing"
+              target="_blank"
+            >
+              <Image src={Spread4} />
+            </a>
+          </div>
+        </div>
         {/* ROW 3 */}
-        <div className={styles.flexRow}></div>
+        <div className={styles.flexRow}>
+        <div
+            data-tip={
+              '<h1>"Droid Font Poster"</h1> <h4> PaintToolSAI </h4> <p>Inspired by Tarkovky\'s Stalker (1979)</p> <h5>Click to see full image</h5>'
+            }
+            data-html={true}
+            className={styles.Spread1 + " " + styles.images}
+          >
+            <a
+              href="https://drive.google.com/file/d/14v2qsZ__RR7-m-zFJi-BLD9GWwjc7PCS/view?usp=sharing"
+              target="_blank"
+            >
+              <Image src={Spread3} />
+            </a>
+          </div>
+          <div
+            data-tip={
+              '<h1>"Droid Font Poster"</h1> <h4> PaintToolSAI </h4> <p>Inspired by Tarkovky\'s Stalker (1979)</p> <h5>Click to see full image</h5>'
+            }
+            data-html={true}
+            className={styles.Spread1 + " " + styles.images}
+          >
+            <a
+              href="https://drive.google.com/file/d/14v2qsZ__RR7-m-zFJi-BLD9GWwjc7PCS/view?usp=sharing"
+              target="_blank"
+            >
+              <Image src={Spread2} />
+            </a>
+          </div>
+        </div>
         {/* ROW 4 */}
         <div className={styles.flexRow}></div>
         {/* ROW 5 */}
