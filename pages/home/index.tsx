39f99ac0f80323525335cd20714fcb20769ad1df
andreavillang <andreavillang@gmail.com>
2023-05-30 00:53:35 +0800

    updates files and splits formal site

---
+++ pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+const Home = () => {
+  return (
+    <>
+      <div>hello world</div>
+    </>
+  )
+}
+
+export default Home
