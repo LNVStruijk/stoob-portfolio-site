1a18ed44b43e1e1d2a5939ecb6718b7c59e576ce
andreavillang <andreavillang@gmail.com>
2023-05-31 17:55:33 +0800

    google sheet added and retro site disclaimer added

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,12 +1,54 @@
 import React from 'react'
+import { google } from 'googleapis'
 import styles from './styles.module.scss'
 
-const Home = () => {
+const Home = ({ data }) => {
   return (
-    <>
-      <div>hello world</div>
-    </>
+    <div className={styles.container}>
+      {data &&
+        data.map((item, index) => (
+          index !== 0 && 
+            <div className={styles.data}>
+              <h1 key={index}>{item.title}</h1>
+              <p key={index}>{item.description}</p>
+            </div>
+        ))
+      }
+    </div>
   )
+}
+
+export async function getServerSideProps() {
+  const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets']})
+
+  const sheets = google.sheets({ version: 'v4', auth })
+
+  const range = `main!A:D`
+
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.NEXT_PUBLIC_GOOGLE_SPREADSHEET_ID,
+    range: range
+  })
+
+  const resData = res.data.values
+
+  let data = resData.map(([
+      title,
+      description,
+      linkToImage,
+      size
+    ]) => ({
+      title,
+      description,
+      linkToImage,
+      size
+    }))
+
+  return {
+    props: {
+      data
+    },
+  }
 }
 
 export default Home
--- pages/retro/index.tsx
+++ pages/retro/index.tsx
@@ -19,142 +19,147 @@ const Retro = () => {
   const [isPaintingsVisible, setPaintingsVisible] = useState(false)
   const [isOtherWorksVisible, setOtherWorksVisible] = useState(false)
 
-  const [crtFilter, setCrtFilter] = useState(true)
-
   return (
     <div className={styles.crt}>
-      {/* <div className={styles.crtSwitch} onClick={()=> {crtFilter ? setCrtFilter(false) : setCrtFilter(true)}}>
-        <p>CRT</p>
-      </div> */}
-
-      {/* {renderMobilePopUp()} */}
       <div className={styles.container}>
-        <section className={styles.shortcuts} id={"Shortcut Rows"}>
-          {/* SHORTCUT ROW 1 */}
-          <div className={styles.shortcutGrid}>
-            <div onClick={() => setIsGalleryVisible(true)}>
-              <Shortcut name={"featured works"} image={"Featured"} />
-            </div>
-            <div onClick={() => setIsAboutMeVisible(true)}>
-              <Shortcut name={"about me"} image={"AboutMe"} />
-            </div>
-            <div onClick={() => setIsContactMeVisible(true)}>
-              <Shortcut name={"contact me"} image={"ContactMe"} />
-            </div>
+        <div className={styles.constructionText}>
+          <div>
+            <h1>The OG retro page is under construction</h1>
+            <img src='/images/shortcuts/doom-laugh.gif'></img>
           </div>
-
-          {/* SHORTCUT ROW 2 */}
-          <div className={styles.shortcutGrid}>
-            <div onClick={() => setTypographyVisible(true)}>
-              <Shortcut name={"typography"} image={"Typography"} />
-            </div>
-            <div onClick={() => setPostersVisible(true)}>
-              <Shortcut name={"posters"} image={"Posters"} />
-            </div>
-            <div onClick={() => setPaintingsVisible(true)}>
-              <Shortcut name={"paintings"} image={"Paintings"} />
-            </div>
-          </div>
-
-          {/* SHORTCUT ROW 3 */}
-          <div className={styles.shortcutGrid}>
-            <a
-              href="https://www.instagram.com/lucas.makes.stuff/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Shortcut name={'say "hi" '} image={"Doom"} />
-            </a>
-
-            <div onClick={() => setOtherWorksVisible(true)}>
-              <Shortcut name={"other works"} image={"OtherWorks"} />
-            </div>
-          </div>
-
-          {/* PLAYER ROW */}
-          {/* <div className={styles.shortcutGrid}>
-            <ReactAudioPlayer src="my_audio_file.ogg" autoPlay controls />
-          </div> */}
-
-          {/* TEST */}
-          <div className={styles.shortcutGrid}></div>
-        </section>
-
-        <section className={styles.windows} id={"Page Windows"}>
-          {/* GALLERY VIEW */}
-          <PageWindow
-            view={"Featured Works"}
-            isVisible={isGalleryVisible}
-            closeWindow={() => setIsGalleryVisible(false)}
+        </div>
+        <video autoPlay={true} muted className={styles.backgroundVideo}>
+          <source
+            src={"/videos/backgroundVideoCompressed.mp4"}
+            type="video/mp4"
           />
-
-          {/* ABOUT ME VIEW */}
-          <PageWindow
-            view={"About Me"}
-            isVisible={isAboutMeVisible}
-            closeWindow={() => setIsAboutMeVisible(false)}
-          />
-          {/* CONTACT ME VIEW */}
-          <PageWindow
-            view={"Contact Me"}
-            isVisible={isContactMeVisible}
-            closeWindow={() => setIsContactMeVisible(false)}
-          />
-
-          {/* TYPOGRAPHY VIEW */}
-          <PageWindow
-            view={"Typography"}
-            isVisible={isTypographyVisible}
-            closeWindow={() => setTypographyVisible(false)}
-          />
-          {/* POSTERS VIEW */}
-          <PageWindow
-            view={"Posters"}
-            isVisible={isPostersVisible}
-            closeWindow={() => setPostersVisible(false)}
-          />
-          {/* PAINTINGS VIEW */}
-          <PageWindow
-            view={"Paintings"}
-            isVisible={isPaintingsVisible}
-            closeWindow={() => setPaintingsVisible(false)}
-          />
-          {/* OTHER WORKS VIEW */}
-          <PageWindow
-            view={"Other Works"}
-            isVisible={isOtherWorksVisible}
-            closeWindow={() => setOtherWorksVisible(false)}
-          />
-        </section>
+        </video>
       </div>
-
-      <video autoPlay={true} muted className={styles.backgroundVideo}>
-        <source
-          src={"/videos/backgroundVideoCompressed.mp4"}
-          type="video/mp4"
-        />
-      </video>
-
-      <StickyFooter
-        currentWindow={
-          isGalleryVisible
-            ? "Featured Works"
-            : isAboutMeVisible
-            ? "About Me"
-            : isContactMeVisible
-            ? "Contact Me"
-            : isTypographyVisible
-            ? "Typography"
-            : isPostersVisible
-            ? "Posters"
-            : isPaintingsVisible
-            ? "Paintings"
-            : isOtherWorksVisible
-            ? "Other Works"
-            : null
-        }
-      />
     </div>
+
+    // <div className={styles.crt}>
+    //   <div className={styles.container}>
+    //     <section className={styles.shortcuts} id={"Shortcut Rows"}>
+    //       {/* SHORTCUT ROW 1 */}
+    //       <div className={styles.shortcutGrid}>
+    //         <div onClick={() => setIsGalleryVisible(true)}>
+    //           <Shortcut name={"featured works"} image={"Featured"} />
+    //         </div>
+    //         <div onClick={() => setIsAboutMeVisible(true)}>
+    //           <Shortcut name={"about me"} image={"AboutMe"} />
+    //         </div>
+    //         <div onClick={() => setIsContactMeVisible(true)}>
+    //           <Shortcut name={"contact me"} image={"ContactMe"} />
+    //         </div>
+    //       </div>
+
+    //       {/* SHORTCUT ROW 2 */}
+    //       <div className={styles.shortcutGrid}>
+    //         <div onClick={() => setTypographyVisible(true)}>
+    //           <Shortcut name={"typography"} image={"Typography"} />
+    //         </div>
+    //         <div onClick={() => setPostersVisible(true)}>
+    //           <Shortcut name={"posters"} image={"Posters"} />
+    //         </div>
+    //         <div onClick={() => setPaintingsVisible(true)}>
+    //           <Shortcut name={"paintings"} image={"Paintings"} />
+    //         </div>
+    //       </div>
+
+    //       {/* SHORTCUT ROW 3 */}
+    //       <div className={styles.shortcutGrid}>
+    //         <a
+    //           href="https://www.instagram.com/lucas.makes.stuff/"
+    //           target="_blank"
+    //           rel="noreferrer"
+    //         >
+    //           <Shortcut name={'say "hi" '} image={"Doom"} />
+    //         </a>
+
+    //         <div onClick={() => setOtherWorksVisible(true)}>
+    //           <Shortcut name={"other works"} image={"OtherWorks"} />
+    //         </div>
+    //       </div>
+
+    //       {/* TEST */}
+    //       <div className={styles.shortcutGrid}></div>
+    //     </section>
+
+    //     <section className={styles.windows} id={"Page Windows"}>
+    //       {/* GALLERY VIEW */}
+    //       <PageWindow
+    //         view={"Featured Works"}
+    //         isVisible={isGalleryVisible}
+    //         closeWindow={() => setIsGalleryVisible(false)}
+    //       />
+
+    //       {/* ABOUT ME VIEW */}
+    //       <PageWindow
+    //         view={"About Me"}
+    //         isVisible={isAboutMeVisible}
+    //         closeWindow={() => setIsAboutMeVisible(false)}
+    //       />
+    //       {/* CONTACT ME VIEW */}
+    //       <PageWindow
+    //         view={"Contact Me"}
+    //         isVisible={isContactMeVisible}
+    //         closeWindow={() => setIsContactMeVisible(false)}
+    //       />
+
+    //       {/* TYPOGRAPHY VIEW */}
+    //       <PageWindow
+    //         view={"Typography"}
+    //         isVisible={isTypographyVisible}
+    //         closeWindow={() => setTypographyVisible(false)}
+    //       />
+    //       {/* POSTERS VIEW */}
+    //       <PageWindow
+    //         view={"Posters"}
+    //         isVisible={isPostersVisible}
+    //         closeWindow={() => setPostersVisible(false)}
+    //       />
+    //       {/* PAINTINGS VIEW */}
+    //       <PageWindow
+    //         view={"Paintings"}
+    //         isVisible={isPaintingsVisible}
+    //         closeWindow={() => setPaintingsVisible(false)}
+    //       />
+    //       {/* OTHER WORKS VIEW */}
+    //       <PageWindow
+    //         view={"Other Works"}
+    //         isVisible={isOtherWorksVisible}
+    //         closeWindow={() => setOtherWorksVisible(false)}
+    //       />
+    //     </section>
+    //   </div>
+
+    //   <video autoPlay={true} muted className={styles.backgroundVideo}>
+    //     <source
+    //       src={"/videos/backgroundVideoCompressed.mp4"}
+    //       type="video/mp4"
+    //     />
+    //   </video>
+
+    //   <StickyFooter
+    //     currentWindow={
+    //       isGalleryVisible
+    //         ? "Featured Works"
+    //         : isAboutMeVisible
+    //         ? "About Me"
+    //         : isContactMeVisible
+    //         ? "Contact Me"
+    //         : isTypographyVisible
+    //         ? "Typography"
+    //         : isPostersVisible
+    //         ? "Posters"
+    //         : isPaintingsVisible
+    //         ? "Paintings"
+    //         : isOtherWorksVisible
+    //         ? "Other Works"
+    //         : null
+    //     }
+    //   />
+    // </div>
   )
 }
 
