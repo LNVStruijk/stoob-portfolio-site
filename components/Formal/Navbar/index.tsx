c821aa8299d3b58f0a53952a8fd81436a9d46231
andreavillang <andreavillang@gmail.com>
2023-10-01 02:00:45 +0800

    edits eslint rules

--- components/Formal/Navbar/index.tsx
+++ components/Formal/Navbar/index.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from 'react'
+import React from 'react'
 import ThemeSwitcher from '../ThemeSwitcher'
 import { Button } from '@nextui-org/react'
 
-const Navbar: FC = () => {
+const Navbar = () => {
   return (
     <div className='container mx-auto flex justify-end items-center py-4 px-4 lg:px-6 mb-4'>
       <div className='flex items-center gap-x-4'>
